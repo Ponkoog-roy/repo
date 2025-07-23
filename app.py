f9324cc2033dc49792946e2c0a709ad4fb6c33ff
Ponkoog-roy <126132052+Ponkoog-roy@users.noreply.github.com>
2025-07-24 01:15:07 +0600

    Restore git

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
-print('Hello')
-print('Hello')
-print('Hello')
-print('Hello')
+a = 4
+b = 7
+x = "python"
+y = "is"
+z = "Awesome"
+k = f"python is not {z}"
+n = "Ironman"
+
+num = [1, 2, 3, 4, 5]
+the = ["apple ", " mango ", "blackbary", "tometo"]
+
+if "apple " in the:
+    print(("tonny stark"+" "+n+" \n")*2)
+else:
+    print(num)
+
+print("hi")
+the .append("Grave")
+the[1] = "orange"
+the.insert(1, "banana")
+
+print(the)
+
+the.extend(num)
+print(the)
+
+the.pop(0)
+the.remove(5)
+print(the)
+
+the.clear()
+print(the)
---
+++ hellow.py
@@ -0,0 +1,7 @@
+
+x=int(input("give the value of x"))
+y=int(input("give the value of y"))
+if x>y:
+    print("x:{x}")
+else:
+    print("y: {y}")
